c43bf4cd4702d6f99cc144fbd6986595c6c98cc0
isaaksmartstubbenedge <isaaks@stubbenedge.com>
2025-02-14 20:48:56 +0000

    https url links.

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,8 +1,8 @@
 export const URL_ROUTES = {
   base: "https://app.jangoro.com",
   www: "https://jangoro.com",
-  api: "http://api.jangoro.com",
-  ai: "http://ai.jangoro.com",
+  api: "https://api.jangoro.com",
+  ai: "https://ai.jangoro.com",
   base_l: "http://localhost:5173",
   www_l: "http://localhost:3000",
   api_l: "http://localhost:4000",
