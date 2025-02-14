02533fafb4afec96c5a79aa76a00d25cca84404f
isaaksmartstubbenedge <isaaks@stubbenedge.com>
2025-02-14 19:44:33 +0000

    Fixed broken urls.

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,8 +1,8 @@
 export const URL_ROUTES = {
-  base: "https://app.jangaro.com",
-  www: "https://jangaro.com",
-  api: "https://api.jangaro.com",
-  ai: "https://ai.jangaro.com",
+  base: "https://app.jangoro.com",
+  www: "https://jangoro.com",
+  api: "https://api.jangoro.com",
+  ai: "https://ai.jangoro.com",
   base_l: "http://localhost:5173",
   www_l: "http://localhost:3000",
   api_l: "http://localhost:4000",
